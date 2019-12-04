11821242639309c1ac1b769beb81eac1dfb95182
Jakub <jakub.suchenek.25@gmail.com>
2019-12-04 09:17:49 +0100

    This one looks easy, let's get started

---
+++ 2019/Day 4/Part 1/Password breaker.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdio> /* printf */
+
+int main()
+{
+    struct t_password
+    {
+        int min = 307237,
+            max = 307237,
+            result = 0;
+    };
+
+    t_password password;
+
+    system("pause");
+    return 0;
+}
